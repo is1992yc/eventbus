5500e12baef518ac76ce11de016d0edc176cdcc7
yican <yican.pan@dragonsoftbravo.com>
2020-03-10 14:05:24 +0800

    init

---
+++ source/src/main/java/com/is1992yc/eventbus/EventbusApplication.java
@@ -0,0 +1,13 @@
+package com.is1992yc.eventbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbusApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/is1992yc/eventbus/EventbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is1992yc.eventbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
